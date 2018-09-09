16a455a013b4b885c7bcc604411a0af2e27030ff
Rohan Agrawal <send2arohan@gmail.com>
2018-09-09 07:43:47 -0700

    Revert "use sparse dd to speed up"
    This reverts commit aa62d57c59b9dc053ebd33d2d1c6c9e6806bee76.

--- system/server.py
+++ system/server.py
@@ -98,7 +98,7 @@ class SDCardDupe(object):
         out.close()
 
         # Run dd command and output status into the progress.info file
-        dd_cmd = "sudo dcfldd conv=sparse bs=4M if=" + img_file
+        dd_cmd = "sudo dcfldd bs=4M if=" + img_file
         dd_cmd += " of=" + " of=".join(devices)
         dd_cmd += " sizeprobe=if statusinterval=1 2>&1 | sudo tee "
         dd_cmd += config_parse['DuplicatorSettings']['Logs'] + "/progress.info"
